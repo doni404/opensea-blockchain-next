8392a49c3d39649b76cf0b6c5afab0ade8f25046
Imam Ghozali <g.imam@global-coding.com>
2022-07-13 10:27:23 +0700

    feat : add an option function to get all listing with typescript sdk

--- pages/collections/[collectionId].js
+++ pages/collections/[collectionId].js
@@ -3,11 +3,11 @@ import NFTCard from '../../components/NFTCard'
 import React, { useEffect, useState, useMemo } from 'react';
 import { useRouter } from 'next/router';
 import { client } from '../../lib/sanityClient';
-import { useNFTCollection } from "@thirdweb-dev/react";
-import { useMarketplace } from '@thirdweb-dev/react'
+import { useNFTCollection,useMarketplaceListings,useMarketplace } from "@thirdweb-dev/react";
 import { CgWebsite } from 'react-icons/cg';
 import { AiOutlineInstagram, AiOutlineTwitter } from 'react-icons/ai';
 import { HiDotsVertical } from 'react-icons/hi';
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
 const style = {
     bannerImageContainer: `h-[25vh] w-screen overflow-hidden flex justify-center items-center`,
@@ -64,29 +64,23 @@ const Collection = () => {
     }, [nftCollection]);
 
     // Initialize marketplace contract by passing in the contract address
-    const marketplaceAddress = "0x422E976aCC779AaCecB65eA1921989E074EC0094";
+    //0x422E976aCC779AaCecB65eA1921989E074EC0094 omni
+    const marketplaceAddress = "0x9363940402E1e6d2EB0bc45872E0F78A2601657B";
     const marketPlaceModule = useMarketplace(marketplaceAddress);
+    //an optional method to get active listing
+    // const sdk = new ThirdwebSDK("rinkeby");
+    // const contract = sdk.getMarketplace(marketplaceAddress);
 
     useEffect(() => {
         if (!marketPlaceModule) return
-            ; (async () => {
-                const listings = await marketPlaceModule.getAll()
+             (async () => {
+                // const listings = await contract.getActiveListings(); an optional method to get active listing
+                const listings = await marketPlaceModule.getAll();
+                console.log('listings ',listings)
                 setListings(listings)
             })()
     }, [marketPlaceModule])
 
-    // const nftMarketplace = sdk.getMarketplace(marketplaceAddress);
-
-    // // Marketplace
-    // const marketPlaceModule = useMemo(() => {
-    //     if (!provider) return
-
-    //     const sdk = new ThirdwebSDK(provider);
-    //     return sdk.getMarketplace(
-    //         '0xA39c3edc3D1867CC3A9b5FEAB7Afd12f41BF0db3'
-    //     )
-    // }, [provider])
-    
     const fetchCollectionData = async (sanityClient = client) => {
         const query = `*[_type == "marketItems" && contractAddress == "${collectionId}"] {
             "profileImageUrl": profileImage.asset->url,
