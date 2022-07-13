4f3c4aff04058ab195a47794df21eadacdfa09a9
Imam Ghozali <g.imam@global-coding.com>
2022-07-13 10:23:54 +0700

    fix : clean code and change address

--- pages/assets/[nftId].js
+++ pages/assets/[nftId].js
@@ -20,11 +20,9 @@ const Nfts = () => {
     const [selectedNft, setSelectedNft] = useState()
     const [listings, setListings] = useState([])
     const router = useRouter()
-
-    console.log("router : ", router)
-
     // get an instance of your own collection contract
-    const nftCollection = useNFTCollection("0xa2835f77e09784dDEA91a5797670e9901152A6DB");
+    // const nftCollection = useNFTCollection("0xa2835f77e09784dDEA91a5797670e9901152A6DB");
+    const nftCollection = useNFTCollection("0xa725ef5784f6CBC5542308E26ACE54b61C3d7d7A");
 
     useEffect(() => {
         if (nftCollection) {
@@ -34,7 +32,6 @@ const Nfts = () => {
                 .then((nfts) => {
                     // Set selected nft
                     const selectedNftItem = nfts.find((nft) => nft.metadata.id.toString() == router.query.nftId)
-
                     setSelectedNft(selectedNftItem)
                 })
                 .catch((error) => {
@@ -44,7 +41,8 @@ const Nfts = () => {
     }, [nftCollection]);
 
     // Initialize marketplace contract by passing in the contract address
-    const marketplaceAddress = "0xA39c3edc3D1867CC3A9b5FEAB7Afd12f41BF0db3";
+    // const marketplaceAddress = "0xA39c3edc3D1867CC3A9b5FEAB7Afd12f41BF0db3";omni
+    const marketplaceAddress = "0x9363940402E1e6d2EB0bc45872E0F78A2601657B";
     const marketPlaceModule = useMarketplace(marketplaceAddress);
 
     // Get all listings in the collection
